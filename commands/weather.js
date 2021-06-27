0c3c27cb89d37dd54aa796db477782918264c911
John <x@mdln.xyz>
2021-06-27 21:00:10 +0200

    modified weather command

--- commands/weather.js
+++ commands/weather.js
@@ -2,28 +2,28 @@ const fetch = require('node-fetch');
 const { MessageEmbed } = require('discord.js');
 
 module.exports = {
-	name: 'vreme',
-	description: 'vreme',
-  args: true,
-  usage: '<city>',
+	name: 'weather',
+	description: 'weather',
+	args: true,
+	usage: '<city>',
 	execute(message, args) {
 		if (!args.length) {
-			message.reply('!vreme <oras>');
+			message.reply('!weather <city>');
 			return;
 		}
-
+		
 		const oras = args.join(' ');
 		const key = process.env['weather_key'];
 		const units = 'metric';
 		const url = `https://api.openweathermap.org/data/2.5/weather?q=${oras}&units=${units}&appid=${key}`;
-
+		
 		async function foo() {
 			try {
 				const res = await fetch(url);
 				const result = await res.json();
-
-
-        console.log(result);
+				
+				
+				console.log(result);
 				let w = {
 					weather: {
 						main: result.weather[0]['main'],
@@ -51,9 +51,9 @@ module.exports = {
 					},
 					name: result.name
 				};
-
+				
 				const grade = '°C';
-
+				
 				const embed = new MessageEmbed()
 				.setColor('#0099ff')
 				.setTitle('Vremea in ' + w.name + ', ' + w.sys.country)
@@ -61,24 +61,24 @@ module.exports = {
 				.setDescription(`${w.main.temp} ${grade}`)
 				.setThumbnail(w.weather.icon)
 				.addFields(
-					{ name: 'Vremea:', value: `${w.weather.description}`, inline: true},
-					{ name: 'Se simte ca:', value: `${w.main.feels_like} ${grade}`, inline: true},
-					{ name: 'Min:', value: `${w.main.temp_min} ${grade}`, inline: true},
-					{ name: 'Max:', value: `${w.main.temp_max} ${grade}`, inline: true},
-					{ name: 'Presiune atm.:', value: `${w.main.pressure}`, inline: true},
-					{ name: 'Umititate:', value: `${w.main.humidity}`, inline: true},
-					{ name: 'Vânt:', value: `Viteză: ${w.wind.speed}`, inline: true },
+					{ name: 'Vremea:', value: `${w.weather.description.toFixed(1)}`, inline: true},
+					{ name: 'Se simte ca:', value: `${w.main.feels_like.toFixed(1)} °C}`, inline: true},
+					{ name: 'Min:', value: `${w.main.temp_min.toFixed(1)} °C}`, inline: true},
+					{ name: 'Max:', value: `${w.main.temp_max.toFixed(1)} °C}`, inline: true},
+					{ name: 'Presiune atm.:', value: `${w.main.pressure.toFixed(1)}`, inline: true},
+					{ name: 'Umititate:', value: `${w.main.humidity.toFixed(1)}`, inline: true},
+					{ name: 'Vânt:', value: `Viteză: ${w.wind.speed.toFixed(1)}`, inline: true },
 					)
-				.setTimestamp()
-				.setFooter('mBot ftw', 'https://i.imgur.com/wSTFkRM.png');
-				message.channel.send(embed);
-				console.log(result);
-			} catch (e) {
-				console.error(e);
-				message.channel.send({files: [process.env.KT]});
-				message.reply('Nu exista orasu\' asta, boule!');
+					.setTimestamp()
+					.setFooter('mBot ftw', 'https://i.imgur.com/wSTFkRM.png');
+					message.channel.send(embed);
+					console.log(result);
+				} catch (e) {
+					console.error(e);
+					message.channel.send({files: [process.env.KT]});
+					message.reply('Nu exista orasu\' asta, boule!');
+				}
 			}
+			let vreme = foo();
 		}
-		let vreme = foo();
-	}
-};
+	};

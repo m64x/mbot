beb0d3f620bc0eb29b1535f6e0e800a1554b5360
John <x@mdln.xyz>
2021-07-07 23:48:10 +0200

    google

--- commands/google.js
+++ commands/google.js
@@ -1,23 +1,35 @@
-const  googleIt = require('google-it');
+const googleIt = require('google-it');
 
 module.exports = {
-    name: 'google',
-    aliases: ['g', 'gagle'],
-    // cooldown: 5,
-    execute(message, args) {
-        if (!args.length) {
-            message.channel.send('!g <search term>');
-            return;
-          }
- 
-          googleIt({'query': args.join()}).then(results => {
-            console.log(results);
-            message.channel.send(JSON.stringify(results));
-          }).catch(e => {
-            console.log('[ERROR] ' + e);
-            // any possible errors that might have occurred (like no Internet connection)
-          });
-                  
-
+  name: 'google',
+  aliases: ['g', 'gagle'],
+  // cooldown: 5,
+  execute(message, args) {
+    if (!args.length) {
+      message.channel.send('!g <search term>');
+      return;
     }
-  };
+
+    // async function foo() {
+      // try {
+        googleIt({'query': args.join(" "), 'disableConsole': true}).then(results => {
+          console.log(results);
+          results.forEach(function(item, index) { 
+            console.log(item);
+            message.channel.send(item.link);
+            // embed.addField((index + 1) + ". " + item.title, "<" + item.link + ">");
+          });
+          message.channel.send(JSON.stringify(results));
+        }).catch(e => {
+          console.log('[ERROR] ' + e);
+          // any possible errors that might have occurred (like no Internet connection)
+        });
+      // } catch(e) {
+        // console.log(e);
+      // }
+    // };
+    
+    // foo();
+      
+  }
+};

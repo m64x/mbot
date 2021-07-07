741df7c27f9ef29a73e3c843efcbd90d1166f2d8
John <x@mdln.xyz>
2021-07-08 00:07:27 +0200

    google 3

--- commands/google.js
+++ commands/google.js
@@ -10,38 +10,35 @@ module.exports = {
       message.channel.send('!g <search term>');
       return;
     }
-
+    
     // async function foo() {
-      // try {
-        const embed = new MessageEmbed()
-				.setColor('#0099ff')
-				.setTitle('Search results for ' + args.join(' '))
-				.setAuthor('mBot', 'https://i.imgur.com/4FuW9or.png', 'https://github.com/m64x/mbot')
-				.setDescription()
-				.addFields(
-					{ name: 'Weather', value: `${w.weather.description}`, inline: true},
-					)
-					.setTimestamp()
-					.setFooter(`mBot ${config['version']}`, 'https://i.imgur.com/4FuW9or.png');
-
-        googleIt({'query': args.join(" "), 'disableConsole': true, 'excludeSites': 'youtube.com'}).then(results => {
-          console.log(results);
-          results.forEach(function(item, index) { 
-            console.log(item);
-            embed.addField({name: item.title, value: item.link, inline: true});
-            // embed.addField((index + 1) + ". " + item.title, "<" + item.link + ">");
-          });
-          message.channel.send(embed);
-        }).catch(e => {
-          console.log('[ERROR] ' + e);
-          // any possible errors that might have occurred (like no Internet connection)
-        });
-      // } catch(e) {
-        // console.log(e);
-      // }
+    // try {
+    const embed = new MessageEmbed()
+    .setColor('#0099ff')
+    .setTitle('Search results for ' + args.join(' '))
+    .setAuthor('mBot', 'https://i.imgur.com/4FuW9or.png', 'https://github.com/m64x/mbot')
+    .setDescription()
+    .setTimestamp()
+    .setFooter(`mBot ${config['version']}`, 'https://i.imgur.com/4FuW9or.png');
+    
+    googleIt({'query': args.join(" "), 'disableConsole': true, 'excludeSites': 'youtube.com'}).then(results => {
+      console.log(results);
+      results.forEach(function(item, index) { 
+        console.log(item);
+        embed.addField({name: item.title, value: item.link, inline: true});
+        // embed.addField((index + 1) + ". " + item.title, "<" + item.link + ">");
+      });
+      message.channel.send(embed);
+    }).catch(e => {
+      console.log('[ERROR] ' + e);
+      // any possible errors that might have occurred (like no Internet connection)
+    });
+    // } catch(e) {
+    // console.log(e);
+    // }
     // };
     
     // foo();
-      
+    
   }
 };

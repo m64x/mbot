392d4ab77cce926c0c099bd9c0d6f404a8cef549
John <x@mdln.xyz>
2021-07-10 14:56:41 +0200

    google.js x2 pagination

--- commands/dex.js
+++ commands/dex.js
@@ -17,7 +17,6 @@ module.exports = {
     args: true,
     usage: '<daily/term>',
     execute(message, args) {
-        
         const term = args.join('+');
         let url;
         
@@ -31,69 +30,69 @@ module.exports = {
             try {
                 const res = await fetch(url);
                 const result = await res.json();
-
+                
                 if (args[0] === 'daily') {
                     message.channel.send('Cuvantul zilei: `'+result.requested.record.word+'`. \nDefinitie: ' + result.requested.record.definition.internalRep);
                     message.channel.send(result.requested.record.image);
-                } else {
-					f.debug(result.definitions[0].internalRep);
+                    return;
+                }
+                
+                f.debug(result.definitions[0].internalRep);
+                
+                const definitionsCount = result.definitions.length;
 
-                    const definitionsCount = result.definitions.length;
-
-					if (definitionsCount) {
-	                    message.channel.send('Există un număr de **' + definitionsCount + ' definitii** pentru termenul **' + term + '**.');
-					}
-
-					let pages = [];
-
-					for (i = 0; i < 5; i++) {
-						pages[i] = new MessageEmbed()
-						.setColor(config.embedColor)
-						.setTitle(`Definiția termenului **${term}**. ${i + 1}/5`)
-						// .setTitle('Definiția termenului ' + term + '. ' + i + '/5')
-						.setAuthor('mBot', 'https://i.imgur.com/wSTFkRM.png')
-						.setDescription(result.definitions[i].internalRep)
-						.addField('Dicționar', result.definitions[i].sourceName, true)
-						.setTimestamp()
-						.setFooter('mBot v0.1.0', 'https://i.imgur.com/wSTFkRM.png');
-					}
-
-    				// let page = 1;
-	    			// let m = `${pages[page - 1]} \n Page ${page} of ${pages.length}.`;
-
-    				let page = 0;
-	    			let m = `${pages[page]} \n Page ${page} of ${pages.length}.`;
-
-		message.channel.send(pages[0]).then(msg => {
-        msg.react(backArrowEmoji);
-        msg.react(forwardArrowEmoji).then(r => {
-
-          // Filters. off:  && user.id === message.author.id
-          // const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅';
-          const backwardsFilter = (reaction) => reaction.emoji.name === backArrowEmoji;
-          const forwardsFilter = (reaction) => reaction.emoji.name === forwardArrowEmoji;
-          const backwards = msg.createReactionCollector(backwardsFilter, { timer: 6000 });
-          const forwards = msg.createReactionCollector(forwardsFilter, { timer: 6000 });
-
-          backwards.on('collect', (r, u) => {
-            if (page === 1) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
-            page--
-            m = `${pages[page - 1]} \n Page ${page} of ${pages.length}`;
-            msg.edit(pages[page]);
-            r.users.remove(r.users.cache.filter(u => u === message.author).first())
-          });
-
-          forwards.on('collect', (r, u) => {
-            if (page === pages.length) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
-            page++
-            m = `${pages[page - 1]} \n Page ${page} of ${pages.length}.`;
-            msg.edit(pages[page-1]);
-            r.users.remove(r.users.cache.filter(u => u === message.author).first());
-          });
-        })
-      })
-
-			}
+                if (definitionsCount) {
+                    message.channel.send('Există un număr de **' + definitionsCount + ' definitii** pentru termenul **' + term + '**.');
+                }
+                
+                let pages = [];
+                
+                for (i = 0; i < 5; i++) {
+                    pages[i] = new MessageEmbed()
+                    .setColor(config.embedColor)
+                    .setTitle(`Definiția termenului **${term}**. ${i + 1}/5`)
+                    .setAuthor('mBot', 'https://i.imgur.com/wSTFkRM.png')
+                    .setDescription(result.definitions[i].internalRep)
+                    .addField('Dicționar', result.definitions[i].sourceName, true)
+                    .setTimestamp()
+                    .setFooter('mBot v0.1.0', 'https://i.imgur.com/wSTFkRM.png');
+                }
+                
+                // let page = 1;
+                // let m = `${pages[page - 1]} \n Page ${page} of ${pages.length}.`;
+                
+                let page = 0;
+                let m = `${pages[page]} \n Page ${page} of ${pages.length}.`;
+                
+                message.channel.send(pages[0]).then(msg => {
+                    msg.react(backArrowEmoji);
+                    msg.react(forwardArrowEmoji).then(r => {
+                        
+                        // Filters. off:  && user.id === message.author.id
+                        // const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅';
+                        const backwardsFilter = (reaction) => reaction.emoji.name === backArrowEmoji;
+                        const forwardsFilter = (reaction) => reaction.emoji.name === forwardArrowEmoji;
+                        const backwards = msg.createReactionCollector(backwardsFilter, { timer: 6000 });
+                        const forwards = msg.createReactionCollector(forwardsFilter, { timer: 6000 });
+                        
+                        backwards.on('collect', (r, u) => {
+                            if (page === 1) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
+                            page--
+                            m = `${pages[page - 1]} \n Page ${page} of ${pages.length}`;
+                            msg.edit(pages[page]);
+                            r.users.remove(r.users.cache.filter(u => u === message.author).first())
+                        });
+                        
+                        forwards.on('collect', (r, u) => {
+                            if (page === pages.length) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
+                            page++
+                            m = `${pages[page - 1]} \n Page ${page} of ${pages.length}.`;
+                            msg.edit(pages[page-1]);
+                            r.users.remove(r.users.cache.filter(u => u === message.author).first());
+                        });
+                    })
+                });
+                
             } catch(e) {
                 console.log(e);
                 message.channel.send(`Termenul ${term} nu există în niciun dicționar.`);
--- commands/google.js
+++ commands/google.js
@@ -1,37 +1,104 @@
 const googleIt = require('google-it');
 const { MessageEmbed } = require('discord.js');
 const config = require('../config.json');
+let paginate = false;
 
 module.exports = {
-  name: 'google',
-  aliases: ['g', 'gagle'],
-  cooldown: 3,
-  execute(message, args) {
-    if (!args.length) {
-      message.channel.send('!g <search term>');
-      return;
-    }
-
-    let embed = new MessageEmbed()
-    .setColor('#0099ff')
-    .setTitle('Search results for `' + args.join(' ') + '`')
-    .setAuthor('mBot', config.botAvatar, 'https://github.com/m64x/mbot')
-    .setDescription()
-    .setTimestamp()
-    .setFooter(`mBot ${config['version']}`, config.botAvatar);
-    
-    googleIt({'query': args.join(" "), 'disableConsole': true, 'excludeSites': 'youtube.com'}).then(results => {
-      console.log(results);
-      results.forEach(function(item, index) { 
-        console.log(item);
-        embed.addField(item.title, item.link);
-        // embed.addField((index + 1) + ". " + item.title, "<" + item.link + ">");
-      });
-      message.channel.send(embed);
-    }).catch(e => {
-      console.log('[ERROR] ' + e);
-      // any possible errors that might have occurred (like no Internet connection)
-    });
-    
-  }
+	name: 'google',
+	aliases: ['g', 'gagle'],
+	cooldown: 3,
+	execute(message, args) {
+		if (!args.length) {
+			message.channel.send('!g <search term>');
+			return;
+		}
+		
+		if (args.length >= 2) {
+			if (args[0] === '!p') {
+				paginate = true;
+				args.shift();
+			}
+		}
+		
+		if (!paginate) {
+			let embed = new MessageEmbed()
+			.setColor('#0099ff')
+			.setTitle('Search results for `' + args.join(' ') + '`')
+			.setAuthor('mBot', config.botAvatar, config.repository)
+			.setDescription()
+			.setTimestamp()
+			.setFooter(`mBot ${config['version']}`, config.botAvatar);
+			
+			googleIt({'query': args.join(" "), 'disableConsole': true, 'excludeSites': 'youtube.com'}).then(results => {
+				results.forEach(function(item, index) {
+					console.log(item);
+					embed.addField(item.title, item.link);
+				});
+				message.channel.send(embed);
+			}).catch(e => {
+				console.log('[ERROR] ' + e);
+			});
+		} else {
+			let pages = [];
+			let page = 0;
+			
+			googleIt({'query': args.join(" "), 'disableConsole': true, 'excludeSites': 'youtube.com'}).then(results => {
+				results.forEach(function(item, index) {
+					pages[index] = new MessageEmbed()
+					.setColor(config.embedColor)
+					.setTitle('#' + index)
+					.setAuthor('mBot', config.botAvatar, config.repository)
+					.addField(item.title, item.link)
+					.setDescription()
+					.setTimestamp()
+					.setFooter(`mBot ${config['version']}`, config.botAvatar);
+				});
+				// message.channel.send(embed);
+			}).catch(e => {
+				console.log('[ERROR] ' + e);
+			});
+			
+			// for (i = 0; i < 10; i++) {
+			// pages[i] = new MessageEmbed()
+			// .setColor(config.embedColor)
+			// .setTitle('Search results for `' + args.join(' ') + '`')
+			// .setAuthor('mBot', config.botAvatar, config.repository)
+			// .setDescription()
+			// .setTimestamp()
+			// .setFooter(`mBot ${config['version']}`, config.botAvatar);
+			// }
+			
+			let m = `${pages[page]} \n Page ${page} of ${pages.length}.`;
+			
+			message.channel.send(pages[0]).then(msg => {
+				msg.react(backArrowEmoji);
+				msg.react(forwardArrowEmoji).then(r => {
+					
+					// Filters. off:  && user.id === message.author.id
+					// const backwardsFilter = (reaction, user) => reaction.emoji.name === '⬅';
+					const backwardsFilter = (reaction) => reaction.emoji.name === backArrowEmoji;
+					const forwardsFilter = (reaction) => reaction.emoji.name === forwardArrowEmoji;
+					const backwards = msg.createReactionCollector(backwardsFilter, { timer: 6000 });
+					const forwards = msg.createReactionCollector(forwardsFilter, { timer: 6000 });
+					
+					backwards.on('collect', (r, u) => {
+						if (page === 1) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
+						page--
+						m = `${pages[page - 1]} \n Page ${page} of ${pages.length}`;
+						msg.edit(pages[page]);
+						r.users.remove(r.users.cache.filter(u => u === message.author).first())
+					});
+					
+					forwards.on('collect', (r, u) => {
+						if (page === pages.length) return r.users.remove(r.users.cache.filter(u => u === message.author).first())
+						page++
+						m = `${pages[page - 1]} \n Page ${page} of ${pages.length}.`;
+						msg.edit(pages[page-1]);
+						r.users.remove(r.users.cache.filter(u => u === message.author).first());
+					});
+				})
+			});
+		}
+		
+	}
 };

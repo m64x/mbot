b88562652f58def571ae6208984562e372e3e719
m64x <mdlnx.7@gmail.com>
2021-11-06 14:26:12 +0200

    id

---
+++ commands/id.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'id',
+    description: 'get user id',
+    execute(message, args) {
+        let x = client.users.cache.find(u => u.tag === 'Mădălin#8114').id
+
+        message.channel.send(x || typeof x);
+    }
+};

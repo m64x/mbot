5aba8c90a52c12b2413c7db2152e43d6359890e1
m64x <mdlnx.7@gmail.com>
2021-11-06 14:59:11 +0200

    id 3

--- commands/id.js
+++ commands/id.js
@@ -2,8 +2,13 @@ module.exports = {
     name: 'id',
     description: 'get user id',
     execute(client, message, args) {
-        let x = client.users.cache.find(u => u.tag === 'Mădălin#8114').id
+        // let x = client.users.cache.find(u => u.tag === 'Mădălin#8114').id
 
-        message.channel.send(x || typeof x);
+        // message.channel.send(x || typeof x);
+
+        message.guild.members.forEach(member => {
+            console.log(member);
+            message.channel.send(member.id);
+        });
     }
 };
